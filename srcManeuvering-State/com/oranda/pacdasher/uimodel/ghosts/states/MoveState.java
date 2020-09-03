00bf920e67b8b7ca234931779e54f20f14f0805f
Γκάγκας Αναστάσιος <tasosggps@gmail.com>
2020-09-03 17:51:56 +0300

    changed structure

--- srcManeuvering-State/com/oranda/pacdasher/uimodel/ghosts/states/MoveState.java
+++
@@ -1,9 +0,0 @@
-package com.oranda.pacdasher.uimodel.ghosts.states;
-
-import com.oranda.pacdasher.uimodel.PosAndDirection;
-import com.oranda.pacdasher.uimodel.ghosts.GhostStrategy;
-import com.oranda.pacdasher.uimodel.ghosts.MoveAttempt;
-
-public interface MoveState {
-    MoveAttempt move(GhostStrategy context, PosAndDirection posAndDirection);
-}
